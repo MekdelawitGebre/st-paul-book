d40197862594bae418014576a4578cca6150b93b
Mekdelawit Gebre <136165989+MekdelawitGebre@users.noreply.github.com>
2025-08-03 01:51:53 +0300

    feat: add favicon image and implemnet

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,27 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "ቅዱስ ጳውሎስ",
   description: "የቅዱስ ጳውሎስን ስራ እና ህይወት የምናወሳበት መጽሐፍ ነው::",
+  icons: {
+    icon: [
+      {
+        url: "/st-paul-book.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+      {
+        url: "/st-paul-book.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+    ],
+    apple: [
+      {
+        url: "/st-paul-book.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
